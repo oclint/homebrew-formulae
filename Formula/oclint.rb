5206862d61bd1ab6a1582b6bb0ab1b3f96f76f02
Ryuichi Saito <ryuichi@ryuichisaito.com>
2017-03-30 08:08:34 -0500

    Upgrade to 0.12

--- Formula/oclint.rb
+++ Formula/oclint.rb
@@ -1,13 +1,13 @@
 class Oclint < Formula
   desc "OCLint static code analysis tool for C, C++, and Objective-C"
   homepage "http://oclint.org"
-  version '0.11.1'
-  sha256 'fd2a0ca67392ca14a57b9f2bebbcaecd7406b3266fe5c95a691d96e77c073054'
-  url "https://github.com/oclint/oclint/releases/download/v#{version}/oclint-#{version}-x86_64-darwin-16.3.0.tar.gz"
+  version '0.12'
+  sha256 '12fda8c59ddd1896880a3055b701850d89550ea686b8d82aa94fffb74220950d'
+  url "https://github.com/oclint/oclint/releases/download/v#{version}/oclint-#{version}-x86_64-darwin-16.5.0.tar.gz"
   head "https://github.com/oclint/oclint.git"
 
   def install
-    clang_version = '3.9.1'
+    clang_version = '4.0.0'
 
     include.install Dir['include/c++'] unless File.directory? "#{include}/c++"
     "#{include}/c++".install Dir['include/c++/v1'] unless File.directory? "#{include}/c++/v1"
